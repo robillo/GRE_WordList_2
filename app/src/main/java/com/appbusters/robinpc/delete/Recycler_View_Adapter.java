328245ae34470a95185b5b5ab1aea1d94e0830c0
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-05 17:06:37 +0530

    ..

--- app/src/main/java/com/appbusters/robinpc/delete/Recycler_View_Adapter.java
+++ app/src/main/java/com/appbusters/robinpc/delete/Recycler_View_Adapter.java
@@ -4,6 +4,9 @@ package com.appbusters.robinpc.delete;
  * Created by Robin Pc on 08-08-2016.
  */
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
