9c139b10c1258ba5d142cf0d1ef50b116279b9f6
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-05 19:17:30 +0530

    DetailActivity basic layout

--- app/src/main/java/com/appbusters/robinpc/delete/DetailActivity.java
+++ app/src/main/java/com/appbusters/robinpc/delete/DetailActivity.java
@@ -6,8 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView word, meaning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        word = (TextView) findViewById(R.id.word);
+        meaning = (TextView) findViewById(R.id.meaning);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
